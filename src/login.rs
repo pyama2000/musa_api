3b2d8add05a6e4c398dd549f474cbc53456cfc37
T.YAMASHITA <8256tako@gmail.com>
2020-01-26 18:49:48 +0900

    :sparkles: Generate session id

--- src/login.rs
+++ src/login.rs
@@ -1,4 +1,19 @@
+use actix_session::Session;
+use actix_web::{web::Query, HttpResponse, Result};
+use base64;
+use rand::{rngs::OsRng, RngCore};
+use serde::{Deserialize, Serialize};
 use spotify_api::authentication::{Scope, SpotifyOAuth};
+
+#[derive(Deserialize)]
+pub struct Identity {
+    code: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct SessionResponse {
+    session_id: Option<String>,
+}
 
 pub async fn get_login_url() -> String {
     let scopes = vec![
@@ -25,4 +40,16 @@ pub async fn get_login_url() -> String {
     let mut oauth = SpotifyOAuth::new();
     oauth.set_scopes(&scopes);
     oauth.generate_auth_url().unwrap()
+}
+
+pub async fn login(code: Query<Identity>, session: Session) -> Result<HttpResponse> {
+    let mut session_id = vec![0u8; 32];
+    OsRng.fill_bytes(&mut session_id);
+
+    let session_id = base64::encode(&session_id);
+
+    session.set("session_id", &session_id)?;
+    session.renew();
+
+    Ok(HttpResponse::Ok().finish())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_redis::RedisSession;
 use actix_web::{
     middleware::{DefaultHeaders, Logger},
-    web::{get, resource},
+    web::{get, post, resource},
     App, HttpServer,
 };
 
@@ -20,6 +20,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(DefaultHeaders::new().header("Access-Control-Allow-Origin", "*"))
             .wrap(Logger::default())
             .service(resource("/auth").route(get().to(login::get_login_url)))
+            .service(resource("/tokens").route(post().to(login::login)))
     })
     .bind("127.0.0.1:8000")?
     .run()
