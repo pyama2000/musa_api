c3e5d231d4693e08fe642f1ef565040edc2890cb
T.YAMASHITA <8256tako@gmail.com>
2020-01-26 22:15:58 +0900

    :sparkles: Establish MySQL connections

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,2 +1,17 @@
+mod login;
 mod models;
 mod schema;
+
+use std::env;
+
+use diesel::{mysql::MysqlConnection, prelude::*};
+use dotenv::dotenv;
+
+pub fn establish_connection() -> MysqlConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    MysqlConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
