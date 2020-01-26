4018e372abfbaa56f384d232cfe0123f1ea615ef
T.YAMASHITA <8256tako@gmail.com>
2020-01-26 16:35:54 +0900

    :sparkles: Connect to Redis server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_redis::RedisSession;
+use actix_web::{middleware, App, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    better_panic::install();
+
+    std::env::set_var("RUST_LOG", "actix_web=info,actix_redis=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(RedisSession::new("127.0.0.1:6379", &[0; 32]))
+            .wrap(middleware::Logger::default())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
