1917aa991c5a16730c428fe827c0c73d62477ad7
T.YAMASHITA <8256tako@gmail.com>
2020-01-27 17:18:14 +0900

    :+1: Setup diesel for postgres

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -1,14 +1,14 @@
 table! {
     credentials (id) {
-        id -> Integer,
+        id -> Int4,
         user_id -> Varchar,
-        token_id -> Integer,
+        token_id -> Int4,
     }
 }
 
 table! {
     tokens (id) {
-        id -> Integer,
+        id -> Int4,
         access_token -> Varchar,
         refresh_token -> Varchar,
     }
@@ -23,4 +23,8 @@ table! {
 joinable!(credentials -> tokens (token_id));
 joinable!(credentials -> users (user_id));
 
-allow_tables_to_appear_in_same_query!(credentials, tokens, users,);
+allow_tables_to_appear_in_same_query!(
+    credentials,
+    tokens,
+    users,
+);
