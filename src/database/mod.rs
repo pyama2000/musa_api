52ec41ab9a25dac1f3ec217ce482e194fd8647c0
T.YAMASHITA <8256tako@gmail.com>
2020-01-27 17:44:38 +0900

    :sparkles: Fix for postgres

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -6,14 +6,19 @@ pub mod user;
 
 use std::env;
 
-use diesel::{prelude::*, MysqlConnection};
+use diesel::prelude::*;
 use dotenv::dotenv;
 
-pub fn establish_connection() -> MysqlConnection {
+pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    MysqlConnection::establish(&database_url)
+    PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+#[test]
+fn test_connection() {
+    establish_connection();
 }
