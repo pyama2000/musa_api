c3ed18ca118494b82ced2e05b8ea26f469e82e79
T.YAMASHITA <8256tako@gmail.com>
2020-01-27 13:57:40 +0900

    :+1: Modify resource `/tokens` to `/login`

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(DefaultHeaders::new().header("Access-Control-Allow-Origin", "*"))
             .wrap(Logger::default())
             .service(resource("/auth").route(get().to(login::get_login_url)))
-            .service(resource("/tokens").route(post().to(login::login)))
+            .service(resource("/login").route(post().to(login::login)))
     })
     .bind("127.0.0.1:8000")?
     .run()
