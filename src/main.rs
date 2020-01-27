c8e806966ad30f0838f1f5221c8c0783048f3424
T.YAMASHITA <8256tako@gmail.com>
2020-01-28 01:24:32 +0900

    :+1: Use env for Redis URL

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 #[macro_use]
 extern crate diesel;
 
+use std::env;
+
 use actix_redis::RedisSession;
 use actix_web::{
     middleware::{DefaultHeaders, Logger},
     web::{get, post, resource},
     App, HttpServer,
 };
+use dotenv::dotenv;
 
 mod database;
 mod login;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
     better_panic::install();
 
-    std::env::set_var("RUST_LOG", "actix_web=info,actix_redis=info");
+    env::set_var("RUST_LOG", "actix_web=info,actix_redis=info");
     env_logger::init();
 
-    HttpServer::new(|| {
+    let redis_url = env::var("REDIS_URL").expect("REDIS_URL must be set");
+
+    HttpServer::new(move || {
         App::new()
-            .wrap(RedisSession::new("127.0.0.1:6379", &[0; 32]))
+            .wrap(RedisSession::new(&redis_url, &[0; 32]))
             .wrap(DefaultHeaders::new().header("Access-Control-Allow-Origin", "*"))
             .wrap(Logger::default())
             .service(resource("/auth").route(get().to(login::get_login_url)))
