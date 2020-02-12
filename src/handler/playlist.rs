445e1eadd955baabe6c0839daa5fcf7983e905cc
T.YAMASHITA <8256tako@gmail.com>
2020-02-12 22:14:51 +0900

    :sparkles: Return playlist data

--- src/handler/playlist.rs
+++ src/handler/playlist.rs
@@ -1,8 +1,16 @@
 use actix_web::{web::Query, HttpResponse, Result};
+use serde::Deserialize;
 use serde_json::json;
 use spotify_api::playlist::PlaylistClient;
 
 use crate::{database, handler::User};
+
+#[derive(Deserialize)]
+pub struct Playlist {
+    playlist_id: String,
+    #[serde(flatten)]
+    user: User,
+}
 
 pub async fn get_playlists(Query(user): Query<User>) -> Result<HttpResponse> {
     let user_id = user.user_id;
@@ -46,6 +54,27 @@ pub async fn get_playlists(Query(user): Query<User>) -> Result<HttpResponse> {
     let json = json!({
         "user_playlists": user_playlists,
         "followed_playlists": followed_playlists,
+    });
+
+    Ok(HttpResponse::Ok().json(json))
+}
+
+pub async fn get_playlist(Query(struct_playlist): Query<Playlist>) -> Result<HttpResponse> {
+    let user_id = struct_playlist.user.user_id;
+
+    let connection = database::establish_connection();
+    let token_id = database::credential::find_token_id_by_user_id(&connection, &user_id)
+        .unwrap()
+        .unwrap();
+    let token = database::token::find_token(&connection, token_id)
+        .unwrap()
+        .unwrap();
+
+
+    let mut client = PlaylistClient::new(&token.access_token, &token.refresh_token);
+    let playlist = client.get_playlist(&struct_playlist.playlist_id, None);
+    let json = json!({
+        "playlist": playlist,
     });
 
     Ok(HttpResponse::Ok().json(json))
--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ async fn main() -> std::io::Result<()> {
             .service(resource("/callback").route(post().to(login::callback)))
             .service(resource("/login").route(post().to(login::login)))
             .service(resource("/playlists").route(get().to(playlist::get_playlists)))
+            .service(resource("/playlist").route(get().to(playlist::get_playlist)))
     })
     .bind("127.0.0.1:8000")?
     .run()
