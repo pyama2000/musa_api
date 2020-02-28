abfb6fbcaa0f616e7b0a9a44b642adfe27a84ae3
T.YAMASHITA <8256tako@gmail.com>
2020-02-28 17:16:25 +0900

    :recycle: Rename to query

--- src/handler/playlist.rs
+++ src/handler/playlist.rs
@@ -57,8 +57,8 @@ pub async fn get_playlists(Query(user): Query<User>) -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().json(json))
 }
 
-pub async fn get_playlist(Query(struct_playlist): Query<Playlist>) -> Result<HttpResponse> {
-    let user_id = struct_playlist.user.user_id;
+pub async fn get_playlist(Query(query): Query<Playlist>) -> Result<HttpResponse> {
+    let user_id = query.user.user_id;
 
     let connection = database::establish_connection();
     let token_id = database::credential::find_token_id_by_user_id(&connection, &user_id)
@@ -68,9 +68,8 @@ pub async fn get_playlist(Query(struct_playlist): Query<Playlist>) -> Result<Htt
         .unwrap()
         .unwrap();
 
-
     let mut client = PlaylistClient::new(&token.access_token, &token.refresh_token);
-    let playlist = client.get_playlist(&struct_playlist.playlist_id, None);
+    let playlist = client.get_playlist(&query.playlist_id, None);
     let json = json!({
         "playlist": playlist,
     });
