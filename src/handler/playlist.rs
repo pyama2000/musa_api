a81b7a87a3cda8e040a8b17e692d5718a6737d94
T.YAMASHITA <8256tako@gmail.com>
2020-06-27 01:49:08 +0900

    :sparkles: Return playlist detail info

--- src/handler/playlist.rs
+++ src/handler/playlist.rs
@@ -1,8 +1,8 @@
 use actix_session::Session;
-use actix_web::{Error, HttpResponse, Responder};
-// use serde::Deserialize;
+use actix_web::{web::Query, Error, HttpResponse, Responder};
+use serde::Deserialize;
 use serde_json::json;
-use spotify_api::{browse::BrowseClient, playlist::PlaylistClient};
+use spotify_api::{browse::BrowseClient, object::Playlist, playlist::PlaylistClient};
 
 // use crate::handler::User;
 
@@ -12,6 +12,11 @@ use spotify_api::{browse::BrowseClient, playlist::PlaylistClient};
 //     #[serde(flatten)]
 //     user: User,
 // }
+
+#[derive(Debug, Deserialize)]
+pub struct GetPlaylistRequest {
+    playlist_id: String,
+}
 
 pub async fn get_playlists(session: Session) -> Result<impl Responder, Error> {
     if session.get::<String>("user_id")?.is_none() {
@@ -27,7 +32,9 @@ pub async fn get_playlists(session: Session) -> Result<impl Responder, Error> {
     let mut user_playlists = Vec::new();
     let mut followed_playlists = Vec::new();
 
-    let playlists = client.get_current_user_playlists(None, None).get_all_items();
+    let playlists = client
+        .get_current_user_playlists(None, None)
+        .get_all_items();
 
     for playlist in playlists {
         let image_url = match playlist.images.first() {
@@ -65,7 +72,10 @@ pub async fn get_featured_playlists(session: Session) -> Result<impl Responder, 
     let refresh_token = session.get::<String>("refresh_token")?.unwrap();
 
     let mut client = BrowseClient::new(&access_token, &refresh_token);
-    let playlists = client.get_featured_playlists(None, None, Some(10), None).1.get_items();
+    let playlists = client
+        .get_featured_playlists(None, None, Some(10), None)
+        .1
+        .get_items();
 
     let mut jsons = Vec::new();
     for playlist in playlists {
@@ -90,29 +100,52 @@ pub async fn get_featured_playlists(session: Session) -> Result<impl Responder, 
     Ok(HttpResponse::Ok().json(response))
 }
 
-// pub async fn get_playlist(Query(query): Query<Playlist>) -> Result<impl Responder, Error> {
-//     let user_id = query.user.user_id;
-// 
-//     let connection = database::establish_connection();
-//     let token_id = database::credential::find_token_id_by_user_id(&connection, &user_id)
-//         .unwrap()
-//         .unwrap();
-//     let token = database::token::find_token(&connection, token_id)
-//         .unwrap()
-//         .unwrap();
-// 
-//     let mut client = PlaylistClient::new(&token.access_token, &token.refresh_token);
-//     let playlist = client.get_playlist(&query.playlist_id, None);
-//     let json = json!({
-//         "playlist": playlist,
-//     });
-// 
-//     Ok(HttpResponse::Ok().json(json))
-// }
-// 
+pub async fn get_playlist(
+    Query(request): Query<GetPlaylistRequest>,
+    session: Session,
+) -> Result<impl Responder, Error> {
+    if session.get::<String>("user_id")?.is_none() {
+        return Ok(HttpResponse::Unauthorized().finish());
+    }
+
+    let access_token = session.get::<String>("access_token")?.unwrap();
+    let refresh_token = session.get::<String>("refresh_token")?.unwrap();
+
+    let mut client = PlaylistClient::new(&access_token, &refresh_token);
+
+    let Playlist {
+        description,
+        followers,
+        id,
+        images,
+        name,
+        owner,
+        uri,
+        ..
+    } = client.get_playlist(&request.playlist_id, None);
+    let response = json!({
+        "playlist": {
+            "description": description,
+            "followers": {
+                "total": followers.unwrap().total,
+            },
+            "id": id,
+            "image": images.first(),
+            "name": name,
+            "owner": {
+                "name": owner.display_name,
+                "id": owner.id,
+            },
+            "uri": uri,
+        },
+    });
+
+    Ok(HttpResponse::Ok().json(response))
+}
+//
 // pub async fn get_tracks(Query(query): Query<Playlist>) -> Result<impl Responder, Error> {
 //     let user_id = query.user.user_id;
-// 
+//
 //     let connection = database::establish_connection();
 //     let token_id = database::credential::find_token_id_by_user_id(&connection, &user_id)
 //         .unwrap()
@@ -120,15 +153,15 @@ pub async fn get_featured_playlists(session: Session) -> Result<impl Responder, 
 //     let token = database::token::find_token(&connection, token_id)
 //         .unwrap()
 //         .unwrap();
-// 
+//
 //     let mut client = PlaylistClient::new(&token.access_token, &token.refresh_token);
 //     let tracks = client
 //         .get_tracks(&query.playlist_id, None, None, None)
 //         .get_all_items();
-// 
+//
 //     let json = json!({
 //         "tracks": tracks,
 //     });
-// 
+//
 //     Ok(HttpResponse::Ok().json(json))
 // }
